90be26c837cfb23c60178e13b1de5af0ff69e077
Cassie Chew <me@cassie.id>
2021-09-15 21:11:34 +1000

    Moves config

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,18 @@ const PORT = process.env.PORT || 4000;
 
 async function startApolloServer() {
   console.log();
-  await createConnection("default");
+  await createConnection({
+    name: "default",
+    type: "postgres",
+    url: process.env.DB_URL,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    synchronize: true,
+    logging: true,
+    entities: ["./src/Entity/User.ts"],
+  });
   const schema = await buildSchema({
     resolvers: [UserResolver],
   });
